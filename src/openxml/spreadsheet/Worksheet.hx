4cad5e3439a275d06e92cf6306d9dc9b7225da41
Kevin Leung <kevinresol@gmail.com>
2018-01-11 14:54:48 +0800

    Worksheet

--- src/openxml/spreadsheet/Worksheet.hx
+++ src/openxml/spreadsheet/Worksheet.hx
@@ -1,50 +1,43 @@
 package openxml.spreadsheet;
+
+import xml.*;
 import openxml.spreadsheet.Cell.A1Reference;
 import openxml.util.IXml;
 import openxml.XmlNameSpaces;
 
-using openxml.util.XmlTools;
+
 /**
  * ...
  * @author Kevin
  */
-class Worksheet implements IXml
-{
+class Worksheet implements IXml {
 	public var parent(default, null):Workbook;
 	public var name:String;
 	public var id:Int;
 	
 	var cells:Map<A1Reference, Cell>;
 	
-	public function new(workbook:Workbook, name:String) 
-	{
+	public function new(workbook:Workbook, name:String) {
 		parent = workbook;
 		this.name = name;
 		cells = new Map();
 	}
 	
-	public function toXml():Xml 
-	{
-		var xml = Xml.createDocument();
-		xml.addProcessingInstruction('xml version="1.0" encoding="UTF-8" standalone="yes"');
-		
-		var worksheet = xml.addElement('worksheet');
-		worksheet.set('xmlns', XmlNameSpaces.spreadsheetml.MAIN);
-		worksheet.set('xmlns:r', XmlNameSpaces.officeDocument.RELATIONSHIPS);
-		
-		
-		
+	public function toXml():Xml {
+		var doc = new Document(
+			new ProcessingInstruction('xml version="1.0" encoding="UTF-8" standalone="yes"'),
+			new Element('worksheet')
+				.setAttribute('xmlns', XmlNameSpaces.spreadsheetml.MAIN)
+				.setAttribute('xmlns:r', XmlNameSpaces.officeDocument.RELATIONSHIPS)
+		);
 		var minRow = 1000000;
 		var maxRow = 0;
 		var minCol = 65536;
 		var maxCol = 0;
 		
-		
-		
 		var rows:Array<Row> = [];
 		
-		for (cell in cells)
-		{
+		for (cell in cells) {
 			var row = rows.filter(function(r) return r.row == cell.row)[0];
 			if (row == null) 
 				rows.push(row = new Row(cell.row));
@@ -57,20 +50,21 @@ class Worksheet implements IXml
 			row.addCell(cell);
 		}
 		
-		var dimension = worksheet.addElement('dimension');
-		dimension.set('ref', A1Reference.createRange(minRow, minCol, maxRow, maxCol));
+		doc.root.add(
+			new Element('dimension')
+				.setAttribute('ref', A1Reference.createRange(minRow, minCol, maxRow, maxCol))
+		);
 		
-		var sheetData = worksheet.addElement('sheetData');
+		var sheetData = new Element('sheetData');
+		doc.root.add(sheetData);
 		
 		rows.sort(function(r1, r2) return r1.row - r2.row);
-		for (row in rows) 
-			sheetData.addChild(row.toXml());
+		for (row in rows) sheetData.add(row.toXml());
 		
-		return xml;
+		return doc.toXml();
 	}
 	
-	public function getCell(row:Int, col:Int):Cell
-	{
+	public function getCell(row:Int, col:Int):Cell {
 		var a1ref = A1Reference.create(row, col);
 		if (!cells.exists(a1ref))
 			cells[a1ref] = new Cell(this, row, col);
